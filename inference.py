d4e69f92d47c2067c36e4b30f493526ba3d24eec
VladimirGl <glazachev.vladimir@gmail.com>
2020-09-03 18:31:32 +0300

    return face_detection back

--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ import json, subprocess, random, string
 from tqdm import tqdm
 from glob import glob
 import torch
-import face_alignment
+import face_detection
 
 
 import audio
@@ -63,7 +63,7 @@ def get_smoothened_boxes(boxes, T):
     return boxes
 
 def face_detect(images):
-    detector = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, 
+    detector = face_detection.FaceAlignment(face_detection.LandmarksType._2D, 
                                             flip_input=False, device=device)
 
     batch_size = args.face_det_batch_size
